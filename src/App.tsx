9cec6f432e27c5df4330eb33f5540cef16fcebb6
João Pedro <joaopedropereira2209@gmail.com>
2026-03-05 21:09:46 -0300

    added app

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,76 @@
 import { useState } from 'react'
 import './App.css'
 import { Card } from './components/card/card';
-import { useFoodData } from './hooks/useFoodData';
-import { CreateModal } from './components/create-modal/create-modal'
+import { useFoodData } from './hooks/useFoodList';
+import { FoodFormModal } from './components/food-form-modal/food-form-modal';
+import { useFoodDelete } from './hooks/useFoodDelete';
+import type { FoodData } from './types/FoodData';
 
 function App() {
-  const {data} = useFoodData();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data } = useFoodData();
 
-  const handleOpenModal = () => {
-    setIsModalOpen(prev => !prev)
-  }
+  const deleteMutation = useFoodDelete();
+
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+  const [selectedFood, setSelectedFood] = useState<FoodData | null>(null);
 
   return (
     <div className="container">
-      <h1>Cardápio</h1>
 
-      <div className="card-grip">
-        {data?.map(foodData => 
-        <Card 
-          price = {foodData.price} 
-          title = {foodData.title} 
-          image = {foodData.image}
+      <header className="header">
+        <div>
+          <h1>Cardápio</h1>
+          <p>Gerencie os produtos do seu restaurante</p>
+        </div>
+
+        <button 
+          className="btn-create"
+          onClick={() => setIsCreateOpen(true)}
+        >
+          + Novo produto
+        </button>
+      </header>
+
+      <main className="content">
+
+        <div className="card-grid">
+          {data?.map(foodData => (
+            <Card 
+              key = {foodData.id}
+              price = {foodData.price} 
+              title = {foodData.title} 
+              image = {foodData.image}
+              onEdit = {() => setSelectedFood(foodData)}
+            onDelete={() => {
+              if(confirm("Deseja deletar esse produto?")) {
+                deleteMutation.mutate(foodData.id!)
+              }
+            }}
+            />
+            ))}
+        </div>
+
+      </main>
+
+      {/*Modal create*/}
+      {isCreateOpen && (
+        <FoodFormModal
+          mode="create"
+          closeModal={() => setIsCreateOpen(false)}
         />
-        )}
-        
-      </div>
+      )}
 
-      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      {/* Modal update */}
+      {selectedFood && (
+        <FoodFormModal
+          mode="update"
+          food={selectedFood}
+          closeModal={() => setSelectedFood(null)}
+        />
+      )}
 
-      <button onClick={handleOpenModal}>Novo</button>
     </div>
-  )
+  );
 }
 
 export default App
